423f424d588b56f9fd981dca3acb32c49f044970
Grant Novota <47872713+Novota15@users.noreply.github.com>
2020-08-04 21:02:20 -0600

    more debug stuff

--- main.cpp
+++ main.cpp
@@ -5,6 +5,30 @@
 // include GLFW - lightweight opengl library
 #include <GLFW/glfw3.h>
 
+// error and debug
+#define GL_ERROR_CASE(glerror)\
+    case glerror: snprintf(error, sizeof(error), "%s", #glerror)
+
+inline void gl_debug(const char *file, int line) {
+    GLenum err;
+    while((err = glGetError()) != GL_NO_ERROR){
+        char error[128];
+
+        switch(err) {
+            GL_ERROR_CASE(GL_INVALID_ENUM); break;
+            GL_ERROR_CASE(GL_INVALID_VALUE); break;
+            GL_ERROR_CASE(GL_INVALID_OPERATION); break;
+            GL_ERROR_CASE(GL_INVALID_FRAMEBUFFER_OPERATION); break;
+            GL_ERROR_CASE(GL_OUT_OF_MEMORY); break;
+            default: snprintf(error, sizeof(error), "%s", "UNKNOWN_ERROR"); break;
+        }
+
+        fprintf(stderr, "%s - %s: %d\n", error, file, line);
+    }
+}
+
+#undef GL_ERROR_CASE
+
 // error events reported through callbacks
 void error_callback(int error, const char* description) {
     fprintf(stderr, "Error: %s\n", description);
@@ -46,7 +70,13 @@ int main(int argc, char* argv[]) {
     int glVersion[2] = {-1,1};
     glGetIntegerv(GL_MAJOR_VERSION, &glVersion[0]);
     glGetIntegerv(GL_MINOR_VERSION, &glVersion[1]);
+
+    gl_debug(__FILE__, __LINE__);
+
+    // report versions used
     printf("Using OpenGL: %d.%d\n", glVersion[0], glVersion[1]);
+    printf("Renderer used: %s\n", glGetString(GL_RENDERER));
+    printf("Shading Language: %s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
 
     // the game loop
     glClearColor(1.0, 0.0, 0.0, 1.0);
