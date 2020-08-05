a1674d55663aac144a6881276ddbe4a9fd770d92
Grant Novota <47872713+Novota15@users.noreply.github.com>
2020-08-04 20:58:47 -0600

    init game window

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
+// standard C IO header
 #include <cstudio>
+// include loading library
+#include <GL/glew.h>
+// include GLFW - lightweight opengl library
 #include <GLFW/glfw3.h>
 
 // error events reported through callbacks
@@ -7,6 +11,54 @@ void error_callback(int error, const char* description) {
 }
 
 int main(int argc, char* argv[]) {
+    // error reporting
     glfwSetErrorCallback(error_callback);
+
+    // initialize the GLFW library
+    if (!glfwInit()) return -1;
+
+    // give GLFW the appropriate hints before creating window
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    // create game window
+    window = glfwCreateWindow(640, 480, "Space Invaders", NULL, NULL);
+    if(!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    // tell GFLW to make subsequent OpenGL calls apply to current 
+    glfwMakeContextCurrent(window);
+
+    // initialize GLEW
+    Glenum err = glewInit();
+    if (err != GLEW_OK) {
+        fprintf(stderr, "Error initializing GLEW.\n");
+        glfwTerminate();
+        return -1
+    }
+
+    // can make calls to opengl functions now
+    // query the opengl version first
+    int glVersion[2] = {-1,1};
+    glGetIntegerv(GL_MAJOR_VERSION, &glVersion[0]);
+    glGetIntegerv(GL_MINOR_VERSION, &glVersion[1]);
+    printf("Using OpenGL: %d.%d\n", glVersion[0], glVersion[1]);
+
+    // the game loop
+    glClearColor(1.0, 0.0, 0.0, 1.0);
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // clean exit
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
     return 0;
 }
