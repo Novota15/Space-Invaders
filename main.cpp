f177649bdfe11142d844dc363546e5f3d5a9e6c7
Grant Novota <47872713+Novota15@users.noreply.github.com>
2020-08-07 21:10:54 -0600

    validation functions

--- main.cpp
+++ main.cpp
@@ -51,6 +51,33 @@ void error_callback(int error, const char* description) {
     fprintf(stderr, "Error: %s\n", description);
 }
 
+void validate_shader(GLuint shader, const char *file = 0) {
+    static const unsigned int BUFFER_SIZE = 512;
+    char buffer[BUFFER_SIZE];
+    GLsizei length = 0;
+
+    glGetShaderInfoLog(shader, BUFFER_SIZE, &length, buffer);
+
+    if(length>0) {
+        printf("Shader %d(%s) compile error: %s\n", shader, (file? file: ""), buffer);
+    }
+}
+
+bool validate_program(GLuint program) {
+    static const GLsizei BUFFER_SIZE = 512;
+    GLchar buffer[BUFFER_SIZE];
+    GLsizei length = 0;
+
+    glGetProgramInfoLog(program, BUFFER_SIZE, &length, buffer);
+
+    if(length>0) {
+        printf("Program %d link error: %s\n", program, buffer);
+        return false;
+    }
+
+    return true;
+}
+
 // clear buffer to a certain color
 void buffer_clear(Buffer* buffer, uint32_t color) {
     for (size_t i = 0; i < buffer->width * buffer->height; ++i) {
