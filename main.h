e1ac946a366fdf0254a0df2280bf4f07a51f6e3e
Grant Novota <47872713+Novota15@users.noreply.github.com>
2020-08-07 23:26:34 -0600

    cleanup and key functionality

--- main.h
+++ main.h
@@ -4,11 +4,11 @@
 
 // standard C IO header
 #include <cstdio>
+#include <cstdint>
 // include loading library
 #include <GL/glew.h>
 // include GLFW - lightweight opengl library
 #include <GLFW/glfw3.h>
-#include <cstdint>
 
 #include "game-objects.h"
 
