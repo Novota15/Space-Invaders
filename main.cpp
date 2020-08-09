9bb45794c5eb46fbefb3d8b9d891624e170c9d5c
Grant Novota <47872713+Novota15@users.noreply.github.com>
2020-08-08 18:03:34 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -51,13 +51,13 @@ double random(uint32_t* rng) {
     return (double)xorshift32(rng) / std::numeric_limits<uint32_t>::max();
 }
 
-GLFWwindow create_game_window(size_t buffer_width, size_t buffer_height) {
+GLFWwindow* create_game_window(size_t buffer_width, size_t buffer_height) {
     GLFWwindow* window = glfwCreateWindow(2 * buffer_width, 2 * buffer_height, "Space Invaders", NULL, NULL);
     if (!window) {
         glfwTerminate();
         // return -1;
     }
-    return *window;
+    return window;
 }
 
 int main(int argc, char* argv[]) {
